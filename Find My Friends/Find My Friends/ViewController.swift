9dc44a8fd46fe978bb1413e466515bf629ef18eb
Phong Nguyen Nam <phongnn_57@vnu.edu.vn>
2015-03-10 16:16:45 +0700

    start create login page

--- Find My Friends/Find My Friends/ViewController.swift
+++ Find My Friends/Find My Friends/ViewController.swift
@@ -10,16 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    // VARIABLES
+    //let util = Utils()
+    let maxIndex = Utils().MAX_VIEW
+    var currentIndex = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currentIndex = 1
+        pageControl.currentPage = 0
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "page1.png")!)
+        addGesture()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    func addGesture() {
+        // left swipe gesture
+        var leftGesture = UISwipeGestureRecognizer(target: self, action: "swipe:")
+        leftGesture.direction = UISwipeGestureRecognizerDirection.Left
+        self.view.addGestureRecognizer(leftGesture)
+        
+        // right swipe gesture
+        var rightGesture = UISwipeGestureRecognizer(target: self, action: "swipe:")
+        rightGesture.direction = UISwipeGestureRecognizerDirection.Right
+        self.view.addGestureRecognizer(rightGesture)
+    }
+    
+    func swipe(sender: UISwipeGestureRecognizer) {
+        
+        if sender.direction == UISwipeGestureRecognizerDirection.Left {
+            if currentIndex == maxIndex {
+                return
+            } else {
+                pageControl.currentPage = currentIndex
+                currentIndex++
+                setImageBackgroundWithImage("page\(currentIndex).png")
+            }
+            println("swipe left")
+        } else if sender.direction == UISwipeGestureRecognizerDirection.Right {
+            if currentIndex == 1 {
+                return
+            } else {
+                pageControl.currentPage = currentIndex
+                currentIndex--
+                setImageBackgroundWithImage("page\(currentIndex).png")
+            }
+            println("swipe right")
+        }
+    }
+    
+    func setImageBackgroundWithImage(imageStr: String) {
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: imageStr)!)
+    }
 
 }
 
