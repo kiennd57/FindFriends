08a408924b2cfee4b1ec98316127e96dd9536fbe
Phong Nguyen Nam <phongnn_57@vnu.edu.vn>
2015-03-19 16:44:18 +0700

    add map

---
+++ Find My Friends/CheckinViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CheckinViewController.swift
+//  Find My Friends
+//
+//  Created by Phong Nguyen Nam on 3/19/15.
+//  Copyright (c) 2015 Nam Phong Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class CheckinViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Find My Friends/Find My Friends/RootViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RootViewController.swift
+//  Find My Friends
+//
+//  Created by Phong Nguyen Nam on 3/18/15.
+//  Copyright (c) 2015 Nam Phong Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
